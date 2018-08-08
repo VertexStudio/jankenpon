00edf1a47c670bf4ef70d326f50ada4cf16379b0
Alex Rozgo <alex.rozgo@gmail.com>
2018-08-08 01:07:07 -0600

    better account names, remove unnecessary steps

--- init_chain.sh
+++ init_chain.sh
@@ -1,24 +1,19 @@
 cleos wallet unlock -n debug --password PW5JGqrHwsAphXLGcZ4rHAJqQxmGQXdL42puB5Vjo14odGspwYFvo
 
 # setup params
-EOS_BUILD_PATH=$HOME/eos/build
 EOS_KEY=EOS8k4wDWyrq9yPUL17YKqThf5F5VVMs5c8diRZQfGKG9698dd6na
 
-# initial chain contracts
-echo "\nBooting Chain Contracts"
-cleos set contract eosio ${EOS_BUILD_PATH}/contracts/eosio.bios -p eosio
-
-# dealer account
-echo "\nCreating dealer account"
-cleos create account eosio dealer ${EOS_KEY} ${EOS_KEY}
-cleos set contract dealer jankenpon -p dealer
+# jankenpon account
+echo "\nCreating jankenpon account"
+cleos create account eosio jankenpon ${EOS_KEY} ${EOS_KEY}
+cleos set contract jankenpon jankenpon -p jankenpon
 
 # Create players accounts
 echo "\nCreating players accounts"
 cleos create account eosio player.jose ${EOS_KEY} ${EOS_KEY}
 cleos create account eosio player.rosa ${EOS_KEY} ${EOS_KEY}
 
-cleos push action dealer create '["player.jose", "player.rosa"]' -p player.rosa
+cleos push action jankenpon create '["player.jose", "player.rosa"]' -p player.rosa
 
 # ./play.sh 1 1 2
 # ./play.sh 2 2 1
--- play.sh
+++ play.sh
@@ -16,7 +16,7 @@ ROUND=$1
 
 echo "------------------------------------------------------------------------------"
 echo "BEFORE"
-cleos get table dealer player.rosa game
+cleos get table jankenpon player.rosa game
 
 echo "------------------------------------------------------------------------------"
 echo "player.rosa commit"
@@ -26,7 +26,7 @@ PLAYER_ROSA_COMMIT_ROUND=$(echo -n $PLAYER_ROSA_SECRET_ROUND | xxd -r -p | sha2 
 echo "PLAYER_ROSA_SECRET_ROUND: $PLAYER_ROSA_SECRET_ROUND"
 echo "PLAYER_ROSA_PLAYED_ROUND: $PLAYER_ROSA_PLAYED_ROUND"
 echo "PLAYER_ROSA_COMMIT_ROUND: $PLAYER_ROSA_COMMIT_ROUND"
-cleos push action dealer commit '["player.jose", "player.rosa", "'"$ROUND"'", "player.rosa", "'"$PLAYER_ROSA_COMMIT_ROUND"'"]' -p player.rosa
+cleos push action jankenpon commit '["player.jose", "player.rosa", "'"$ROUND"'", "player.rosa", "'"$PLAYER_ROSA_COMMIT_ROUND"'"]' -p player.rosa
 
 echo "------------------------------------------------------------------------------"
 echo "player.jose commit"
@@ -36,16 +36,16 @@ PLAYER_JOSE_COMMIT_ROUND=$(echo -n $PLAYER_JOSE_SECRET_ROUND | xxd -r -p | sha2 
 echo "PLAYER_JOSE_SECRET_ROUND: $PLAYER_JOSE_SECRET_ROUND"
 echo "PLAYER_JOSE_PLAYED_ROUND: $PLAYER_JOSE_PLAYED_ROUND"
 echo "PLAYER_JOSE_COMMIT_ROUND: $PLAYER_JOSE_COMMIT_ROUND"
-cleos push action dealer commit '["player.jose", "player.rosa", "'"$ROUND"'", "player.jose", "'"$PLAYER_JOSE_COMMIT_ROUND"'"]' -p player.jose
+cleos push action jankenpon commit '["player.jose", "player.rosa", "'"$ROUND"'", "player.jose", "'"$PLAYER_JOSE_COMMIT_ROUND"'"]' -p player.jose
 
 echo "------------------------------------------------------------------------------"
 echo "player.rosa reveal"
-cleos push action dealer reveal '["player.jose", "player.rosa", "'"$ROUND"'", "player.rosa", "'"$PLAYER_ROSA_SECRET_ROUND"'", "'"$PLAYER_ROSA_PLAYED_ROUND"'"]' -p player.rosa
+cleos push action jankenpon reveal '["player.jose", "player.rosa", "'"$ROUND"'", "player.rosa", "'"$PLAYER_ROSA_SECRET_ROUND"'", "'"$PLAYER_ROSA_PLAYED_ROUND"'"]' -p player.rosa
 
 echo "------------------------------------------------------------------------------"
 echo "player.jose reveal"
-cleos push action dealer reveal '["player.jose", "player.rosa", "'"$ROUND"'", "player.jose", "'"$PLAYER_JOSE_SECRET_ROUND"'", "'"$PLAYER_JOSE_PLAYED_ROUND"'"]' -p player.jose
+cleos push action jankenpon reveal '["player.jose", "player.rosa", "'"$ROUND"'", "player.jose", "'"$PLAYER_JOSE_SECRET_ROUND"'", "'"$PLAYER_JOSE_PLAYED_ROUND"'"]' -p player.jose
 
 echo "------------------------------------------------------------------------------"
 echo "AFTER"
-cleos get table dealer player.rosa game
+cleos get table jankenpon player.rosa game
