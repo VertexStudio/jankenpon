4f798633468c058472d4363e6831535dc4807e95
racsoraul <racsoraul@gmail.com>
2018-11-20 12:35:13 -0600

    Smart contract upgraded to eosio.cdt 1.4.1

--- jankenpon.cpp
+++ jankenpon.cpp
@@ -14,15 +14,15 @@ uint8_t plays[3][3] = {
 
 using namespace eosio;
 
-class jankenpon : public eosio::contract
+class [[eosio::contract("jankenpon")]] Jankenpon : public eosio::contract
 {
   private:
-    static bool is_equal(const checksum256 &a, const checksum256 &b)
+    static bool is_equal(const capi_checksum256 &a, const capi_checksum256 &b)
     {
-        return memcmp((void *)&a, (const void *)&b, sizeof(checksum256)) == 0;
+        return memcmp((void *)&a, (const void *)&b, sizeof(capi_checksum256)) == 0;
     }
 
-    static bool is_zero(const checksum256 &a)
+    static bool is_zero(const capi_checksum256 &a)
     {
         const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&a);
         return p64[0] == 0 && p64[1] == 0 && p64[2] == 0 && p64[3] == 0;
@@ -31,56 +31,56 @@ class jankenpon : public eosio::contract
   public:
     using contract::contract;
 
-    jankenpon(account_name self) : contract(self), games(_self, _self) {}
+    Jankenpon(name self, name code, datastream<const char*> ds) 
+        : contract(self, code, ds), games(self, code.value) {}
 
-    //@abi table
-    struct game
+    struct [[eosio::table]] game
     {
-        account_name peer;
-        account_name host;
+        uint64_t peer;
+        uint64_t host;
         uint8_t peerwins;
         uint8_t hostwins;
         uint8_t round;
-        checksum256 peercommit;
-        checksum256 hostcommit;
-        checksum256 peersecret;
-        checksum256 hostsecret;
+        capi_checksum256 peercommit;
+        capi_checksum256 hostcommit;
+        capi_checksum256 peersecret;
+        capi_checksum256 hostsecret;
         auto primary_key() const { return peer; }
         EOSLIB_SERIALIZE(game, (peer)(host)(peerwins)(hostwins)(round)(peercommit)(hostcommit)(peersecret)(hostsecret))
     };
 
-    typedef eosio::multi_index<N(game), game> game_index;
+    typedef eosio::multi_index<name("games"), game> game_index;
 
     game_index games;
 
-    //@abi action
-    void create(const account_name &peer, const account_name &host)
+    [[eosio::action]]
+    void create(const name &peer, const name &host)
     {
         require_auth(host);
         eosio_assert(peer != host, "peer shouldn't be the same as host");
-        game_index existing_host_games(_self, host);
-        auto itr = existing_host_games.find(peer);
+        game_index existing_host_games(_self, host.value);
+        auto itr = existing_host_games.find(peer.value);
         eosio_assert(itr == existing_host_games.end(), "game already exists");
         existing_host_games.emplace(host, [&](auto &g) {
-            g.peer = peer;
-            g.host = host;
+            g.peer = peer.value;
+            g.host = host.value;
             g.peerwins = 0;
             g.hostwins = 0;
             g.round = 1;
-            memset(&g.peercommit, 0, sizeof(checksum256));
-            memset(&g.hostcommit, 0, sizeof(checksum256));
-            memset(&g.peersecret, 0, sizeof(checksum256));
-            memset(&g.hostsecret, 0, sizeof(checksum256));
+            memset(&g.peercommit, 0, sizeof(capi_checksum256));
+            memset(&g.hostcommit, 0, sizeof(capi_checksum256));
+            memset(&g.peersecret, 0, sizeof(capi_checksum256));
+            memset(&g.hostsecret, 0, sizeof(capi_checksum256));
         });
     }
 
-    //@abi commit
-    void commit(const account_name &peer, const account_name &host, uint8_t round, const account_name &player, const checksum256 &commitment)
+    [[eosio::action]]
+    void commit(const name &peer, const name &host, uint8_t round, const name &player, const capi_checksum256 &commitment)
     {
         require_auth(player);
         eosio_assert(peer != host, "peer shouldn't be the same as host");
-        game_index existing_host_games(_self, host);
-        auto itr = existing_host_games.find(peer);
+        game_index existing_host_games(_self, host.value);
+        auto itr = existing_host_games.find(peer.value);
         eosio_assert(itr != existing_host_games.end(), "game doesn't exist");
         existing_host_games.modify(itr, host, [&](auto &g) {
             eosio_assert((g.round == round), "invalid round");
@@ -97,13 +97,13 @@ class jankenpon : public eosio::contract
         });
     }
 
-    //@abi reveal
-    void reveal(const account_name &peer, const account_name &host, uint8_t round, const account_name &player, const checksum256 &secret)
+    [[eosio::action]]
+    void reveal(const name &peer, const name &host, uint8_t round, const name &player, const capi_checksum256 &secret)
     {
         require_auth(player);
         eosio_assert(peer != host, "peer shouldn't be the same as host");
-        game_index existing_host_games(_self, host);
-        auto itr = existing_host_games.find(peer);
+        game_index existing_host_games(_self, host.value);
+        auto itr = existing_host_games.find(peer.value);
         eosio_assert(itr != existing_host_games.end(), "game doesn't exist");
         uint8_t play = ((uint8_t *)&secret)[31];
         eosio_assert((play >= 1 && play <= 3), "invalid play");
@@ -113,14 +113,14 @@ class jankenpon : public eosio::contract
             {
                 eosio_assert(!is_zero(g.peercommit), "peer hasn't committed");
                 eosio_assert(is_zero(g.peersecret), "peer already revealed");
-                assert_sha256((char *)&secret, sizeof(secret), (const checksum256 *)&g.peercommit);
+                assert_sha256((char *)&secret, sizeof(secret), (const capi_checksum256 *)&g.peercommit);
                 g.peersecret = secret;
             }
             else if (player == host)
             {
                 eosio_assert(!is_zero(g.hostcommit), "host hasn't committed");
                 eosio_assert(is_zero(g.hostsecret), "host already revealed");
-                assert_sha256((char *)&secret, sizeof(secret), (const checksum256 *)&g.hostcommit);
+                assert_sha256((char *)&secret, sizeof(secret), (const capi_checksum256 *)&g.hostcommit);
                 g.hostsecret = secret;
             }
             if (!is_zero(g.peersecret) && !is_zero(g.hostsecret))
@@ -130,13 +130,13 @@ class jankenpon : public eosio::contract
                 uint8_t hostplay = ((uint8_t *)&g.hostsecret)[31];
                 g.peerwins += peerplay == plays[peerplay - 1][hostplay - 1] ? 1 : 0;
                 g.hostwins += hostplay == plays[peerplay - 1][hostplay - 1] ? 1 : 0;
-                memset(&g.peercommit, 0, sizeof(checksum256));
-                memset(&g.hostcommit, 0, sizeof(checksum256));
-                memset(&g.peersecret, 0, sizeof(checksum256));
-                memset(&g.hostsecret, 0, sizeof(checksum256));
+                memset(&g.peercommit, 0, sizeof(capi_checksum256));
+                memset(&g.hostcommit, 0, sizeof(capi_checksum256));
+                memset(&g.peersecret, 0, sizeof(capi_checksum256));
+                memset(&g.hostsecret, 0, sizeof(capi_checksum256));
             }
         });
     }
 };
 
-EOSIO_ABI(jankenpon, (create)(commit)(reveal))
+EOSIO_DISPATCH(Jankenpon, (create)(commit)(reveal))
